5453a57707975262a3dd7ffd494cd44e4f55c6f0
lucabarcelo <63828912+lucabarcelo@users.noreply.github.com>
2020-05-05 19:14:11 -0400

    first time returning from html file

--- ljb2179-final/app.py
+++ ljb2179-final/app.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Apr 21 14:57:17 2020
 
-@author: etill
+@author: lbarcelo
 """
 
 #import statements
@@ -14,7 +14,7 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 #start the server
 if __name__ == "__main__":
