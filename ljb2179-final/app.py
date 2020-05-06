b2632eab354f33eed4867fcbcbf7b385fc9ee51d
lucabarcelo <63828912+lucabarcelo@users.noreply.github.com>
2020-05-06 12:02:19 -0400

    added links and image

--- ljb2179-final/app.py
+++ ljb2179-final/app.py
@@ -4,18 +4,31 @@ Created on Tue Apr 21 14:57:17 2020
 
 @author: lbarcelo
 """
-
 #import statements
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
+import banna
 
 #Flask app variable
 app = Flask(__name__)
 
 #static route
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+	return render_template("index.html")
+
+@app.route("/1006")
+def testing1():
+	return render_template("engi.html")
+
+@app.route("/test")
+def testing2():
+	return banna.bannality()
+
+@app.route("/210")
+def redirtest():
+    return redirect("/1006")
 
 #start the server
 if __name__ == "__main__":
     app.run()
+
