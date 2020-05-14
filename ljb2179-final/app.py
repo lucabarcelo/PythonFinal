64f7b6c443ec3c23cc835b893b9292e05d3e162c
lucabarcelo <63828912+lucabarcelo@users.noreply.github.com>
2020-05-13 20:03:20 -0400

    update /crypto path with fully developed hashing
    Added an extra route in order to take care of the hash to word dictionary scan that was implemented.

--- ljb2179-final/app.py
+++ ljb2179-final/app.py
@@ -22,29 +22,30 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-
 @app.route("/crypto", methods=["GET", "POST"])
 def crypto():
     if request.method == "POST":
-        InputHash = request.form["hash"]
-        
-        passlist = open('/Users/lucabarcelo/Desktop/Dev/1006/final/10kPasswords.txt', 'r')
-        readPasslist = passlist.read().split('\n')
-        
-        for word in readPasslist:
-            enc_wrd = word.encode('utf-8')
-            digested = hashlib.sha1(enc_wrd).hexdigest()
-            
-            if digested == InputHash:
-                print("string found")
-                print("string is " + word)
-                return render_template('Cracked.html', word=word)
-                
-            
-            elif digested != InputHash:
-                print('String ' + word + ' doesn\'t match, trying next...')
-                
+        InputWord = request.form["word"]
+        HshInWrd = hashlib.sha1(bytes(InputWord, 'utf-8')).hexdigest()
+        return render_template('Cracked.html', HshInWrd=HshInWrd)
+
     return render_template("crypto.html")
+
+@app.route("/UnhashCrypto", methods=['GET','POST'])
+def UnhashIt():
+    if request.method == 'POST':
+        InputHash = request.form["hash"]
+        OCompList = open('/home/lbarcelo/mysite/500k.txt', 'r')
+        CompList = OCompList.read().split('\n')
+
+        for word in CompList:
+            encwrd = word.encode('utf-8')
+            HashOutWord = hashlib.sha1(encwrd).hexdigest()
+
+            if HashOutWord == InputHash:
+                return render_template('Cracked2.html', word=word)
+
+    return render_template('crypto2.html')
 
 @app.route("/NYCh20Consumption")
 def waterConsump():
@@ -52,14 +53,14 @@ def waterConsump():
 
 @app.route('/200')
 def dataTable():
-    df = pd.read_csv("/Users/lucabarcelo/Desktop/Dev/1006/final/Water_Consumption_In_The_New_York_City.csv")
+    df = pd.read_csv("/home/lbarcelo/mysite/Water_Consumption_In_The_New_York_City.csv")
     return render_template("dataT.html", tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 @app.route("/210")
 def NYCWaterAnalysis():
-    xr = pd.read_csv("/Users/lucabarcelo/Desktop/Dev/1006/final/Water_Consumption_In_The_New_York_City.csv") 
+    xr = pd.read_csv("/home/lbarcelo/mysite/Water_Consumption_In_The_New_York_City.csv")
     total, year = xr['NYC Consumption(Million gallons per day)'], xr['Year']
-    
+
     def give_me_the_straight_boi(x, y):
         w, b = np.polyfit(x,y,deg=1)
         line = w * x + b
@@ -68,34 +69,34 @@ def NYCWaterAnalysis():
     def give_me_change(x, y):
         w, b = np.polyfit(x,y,deg=1)
         return w
-    
+
     change = give_me_change(year, total)
     line = give_me_the_straight_boi(year, total)
-    
+
     fig = Figure()
     ax = fig.subplots()
     ax.scatter(year, line, s=5, color='red', label="40-Year Average \n Total Consumption \n Rate Change:\n  {} Million Gallons".format(round(change, 2)))
     ax.plot(year, total)
     ax.legend()
-    
+
     ax.set_xlim(1978,2020)
     ax.set_ylim(700, 2000)
     ax.set_xlabel('Year')
-    ax.set_ylabel('Daily Consumption per Millions of Gallons') 
+    ax.set_ylabel('Daily Consumption per Millions of Gallons')
     ax.set_title('40-Year NYC H2O Consumption')
 
-        
+
     buf = BytesIO()
     fig.savefig(buf, format="png")
-    
+
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return "<img src='data:image/png;base64,{}'/>".format(data)
 
 @app.route("/220")
 def NYCWaterAnalysises():
-    xr = pd.read_csv("/Users/lucabarcelo/Desktop/Dev/1006/final/Water_Consumption_In_The_New_York_City.csv") 
+    xr = pd.read_csv("/home/lbarcelo/mysite/Water_Consumption_In_The_New_York_City.csv")
     percap, year = xr['Per Capita(Gallons per person per day)'], xr['Year']
-    
+
     def give_me_the_straight_boi(x, y):
         w, b = np.polyfit(x,y,deg=1)
         line = w * x + b
@@ -104,34 +105,34 @@ def NYCWaterAnalysises():
     def give_me_change(x, y):
         w, b = np.polyfit(x,y,deg=1)
         return w
-    
+
     change = give_me_change(year, percap)
     line = give_me_the_straight_boi(year, percap)
-    
+
     fig = Figure()
     ax = fig.subplots()
     ax.scatter(year, line, s=5, color='red', label="40-Year Average \n Individual Consumption \n Rate Change:\n  {} Million Gallons".format(round(change, 2)))
     ax.plot(year, percap)
     ax.legend()
-    
+
     ax.set_xlim(1978,2020)
     ax.set_ylim(50, 250)
     ax.set_xlabel('Year')
-    ax.set_ylabel('Daily Percap Consumption (Gallons)') 
+    ax.set_ylabel('Daily Percap Consumption (Gallons)')
     ax.set_title('40-Year NYC H2O Consumption')
 
-        
+
     buf = BytesIO()
     fig.savefig(buf, format="png")
-    
+
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return "<img src='data:image/png;base64,{}'/>".format(data)
 
 @app.route("/230")
 def NYCWaterAnalysiseses():
-    xr = pd.read_csv("/Users/lucabarcelo/Desktop/Dev/1006/final/Water_Consumption_In_The_New_York_City.csv") 
+    xr = pd.read_csv("/home/lbarcelo/mysite/Water_Consumption_In_The_New_York_City.csv")
     percap, populat = xr['Per Capita(Gallons per person per day)'], xr['New York City Population']
-    
+
     def give_me_the_straight_boi(x, y):
         w, b = np.polyfit(x,y,deg=1)
         line = w * x + b
@@ -140,31 +141,30 @@ def NYCWaterAnalysiseses():
     def give_me_change(x, y):
         w, b = np.polyfit(x,y,deg=1)
         return w
-    
+
     changePer = give_me_change(populat, percap)
     #changeTot = give_me_change(populat, total)
     linePer = give_me_the_straight_boi(populat, percap)
     #lineTot = give_me_the_straight_boi(populat, total)
-    
+
     fig = Figure()
     ax = fig.subplots()
     ax.scatter(populat, linePer, s=5, color='red', label="For Each Person\nAdded to NYC:\n{} per-Capita Gallon change".format(round(changePer, 5)))
     ax.plot(populat, percap)
     ax.legend()
-    
+
     ax.set_xlim(7000000, 8500000)
     ax.set_ylim(50, 250)
     ax.set_xlabel('Population')
-    ax.set_ylabel('Daily Percap Consumption (Gallons)') 
+    ax.set_ylabel('Daily Percap Consumption (Gallons)')
     ax.set_title('40-Year NYC H2O Consumption')
 
-        
+
     buf = BytesIO()
     fig.savefig(buf, format="png")
-    
+
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return "<img src='data:image/png;base64,{}'/>".format(data)
-
 #start the server
 if __name__ == "__main__":
     app.run()
